4485c2d1f724ee73832e243c1cce4a58619cf062
Andyequisde <andyrevollopro@gmail.com>
2022-05-12 15:43:02 -0300

    c
    c

--- TPBolillero.Test/TestDeUnidad.cs
+++ TPBolillero.Test/TestDeUnidad.cs
@@ -1,12 +1,15 @@
 using System;
+using TPBolillero.Core;
 using Xunit;
 
 namespace TPBolillero.Test
 {
-    public class UnitTest1
+    public class TestDeUnidad
     {
+        public Bolillero Bolillero;
+        
         [Fact]
-        public void Test1()
+        public void ()
         {
 
         }
